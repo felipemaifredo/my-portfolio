e3590a4fe0eacb6be357d342bafe3dd4b1f7614c
felipemaifredo <felipemaifredo@gmail.com>
2023-04-26 18:19:23 -0300

    att

--- src/components/MatrixRain.js
+++ src/components/MatrixRain.js
@@ -1,48 +1,49 @@
 import React, { useRef, useEffect } from "react";
 
-const renderMatrix = (ref) => {
-    let canvas = ref.current;
-    let context = canvas.getContext("2d");
+const MatrixRain = () => {
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    const alphabet = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトホモヨョロヲゴゾドボポヴッンABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    const fontSize = 16;
-    const columns = canvas.width / fontSize;
-
-    const rainDrops = [];
-
-    for (let x = 0; x < columns; x++) {
-        rainDrops[x] = 1;
-    }
-
-    const render = () => {
-        context.fillStyle = "rgba(0, 0, 0, 0.05)";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = "#0F0"; // pure green
-        context.font = fontSize + "px monospace";
-        for (let i = 0; i < rainDrops.length; i++) {
-
-            const text = alphabet.charAt( Math.floor(Math.random() * alphabet.length ));
-
-            context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
-
-            if ( rainDrops[i] * fontSize > canvas.height && Math.random() > 0.98 ) {
-                rainDrops[i] = 0;
-            } 
-            
-            rainDrops[i]++;
+    const renderMatrix = (ref) => {
+        let canvas = ref.current;
+        let context = canvas.getContext("2d");
+    
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    
+        const alphabet = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトホモヨョロヲゴゾドボポヴッンABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+        const fontSize = 16;
+        const columns = canvas.width / fontSize;
+    
+        const rainDrops = [];
+    
+        for (let x = 0; x < columns; x++) {
+            rainDrops[x] = 1;
+        }
+    
+        const render = () => {
+            context.fillStyle = "rgba(0, 0, 0, 0.05)";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            context.fillStyle = "#0F0"; // pure green
+            context.font = fontSize + "px monospace";
+            for (let i = 0; i < rainDrops.length; i++) {
+    
+                const text = alphabet.charAt( Math.floor(Math.random() * alphabet.length ));
+    
+                context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+    
+                if ( rainDrops[i] * fontSize > canvas.height && Math.random() > 0.98 ) {
+                    rainDrops[i] = 0;
+                } 
+                
+                rainDrops[i]++;
+            };
+        };
+    
+        return () => {
+            setInterval(render, 60);
         };
     };
 
-    return () => {
-        setInterval(render, 60);
-    };
-};
-
-const MatrixRain = () => {
     const ref = useRef();
     useEffect(() => renderMatrix(ref));
 
